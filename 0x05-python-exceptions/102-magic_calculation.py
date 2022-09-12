5936d43372c89722687254c6595efb2a0352498d
your name <you@example.com>
2022-09-12 04:33:09 -0700

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-
-
 def magic_calculation(a, b):
-    """....."""
-    return (result)
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return result
