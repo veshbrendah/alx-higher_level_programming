03ecc2b05522954bf9b5e4fdc75ec02fe87c3554
your name <you@example.com>
2022-09-12 04:25:53 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+# 1-safe_print_integer.py
 
 
 def safe_print_integer(value):
+    """prints int"""
     try:
         print("{:d}".format(value))
-        return True
-    except:
-        return False
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
