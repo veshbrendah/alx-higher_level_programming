a34bbe6cd929cef284f743b66d4aa4fb8f6cfed0
your name <you@example.com>
2022-09-20 04:38:13 -0700

    all

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")]
+magic_string = __import__('100-magic_string').magic_string
+
+for i in range(10):
+    print(magic_string())
