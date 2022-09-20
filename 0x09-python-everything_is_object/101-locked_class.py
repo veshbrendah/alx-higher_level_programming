e2b91bb8468bdf87f754370a15e34110d397d153
your name <you@example.com>
2022-09-20 04:48:50 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
+
+
+class LockedClass:
     """
-    Definition for a LockedClass class.
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
     """
 
-
-    class LockedClass:
-        """LockedClass class that defines 1 attribute"""
-
-        __slots__ = ['first_name']
+    __slots__ = ["first_name"]
