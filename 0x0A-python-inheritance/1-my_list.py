7305e68283f9aa2deda888bb8fac03b433321ffb
your name <you@example.com>
2022-09-26 05:15:16 -0700

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
-Contains definiton for the class MyList that inherits from list.
+    1-my_list: class MyList
 """
 
 
 class MyList(list):
-    """definiton for the class MyList that inherits from list.
+    """
+        This class inherits from list.
+        Attributes:
+        Methods:
+            print_sorted - prints the list in ascending order
     """
     def print_sorted(self):
-        """Prints list elements(int) in ascending order"""
-
-        sortedlist = sorted(self)
-        print(sortedlist)
+        """
+           prints a list in ascending order.
+        """
+        print(sorted(self))
