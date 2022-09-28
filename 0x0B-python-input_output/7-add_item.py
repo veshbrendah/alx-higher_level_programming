0c0d09e676db35f0467dc26bfd5400fb1ef49f27
your name <you@example.com>
2022-09-28 01:07:22 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""module for task 7"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-dfile = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    with open(dfile, 'r') as f:
-        num = len(f.read())
-except FileNotFoundError:
-    f = open(dfile, 'w')
-    f.close()
-    num = 0
-
-if num == 0:
-    dlist = []
-else:
-    obj = load_from_json_file(dfile)
-    dlist = obj
-
-dlist = dlist + sys.argv[1:]
-
-save_to_json_file(dlist, dfile)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
