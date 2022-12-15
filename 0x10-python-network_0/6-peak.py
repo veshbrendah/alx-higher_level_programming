33e7b23c31fd2e1caa6797c9fc0a90e5e318a5ec
vesh Brendah <veshbrendah9@gmail.com>
2022-12-15 02:25:35 -0800

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""task 6 modele"""
+""" Finds peak in list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
-        return None
+    """Find highest value in list of unsorted integers"""
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    my_list = list_of_integers
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    if my_list:
+        my_list.sort()
+        return my_list[-1]
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return None
