7880bc59be392b65cfdeea5b29d59b84f520f76a
veshbrendah <veshbrendah9@gmail.com>
2023-01-17 12:53:02 +0000

    all

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+t fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
+});
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+t request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
